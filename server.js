4353a07700a72462e11131f4afe45e2ede98bac5
Joel Vargas Reynoso <76189923+joelvshimself@users.noreply.github.com>
2024-10-25 11:01:01 -0600

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,15 @@ const app = express();
 // Sirve archivos estáticos desde la carpeta 'dist'
 app.use(express.static(path.join(__dirname, 'dist')));
 
-// Redirige todas las rutas a index.html para aplicaciones SPA
+// Asegurarse de que los archivos .js tengan el tipo MIME correcto
+app.use((req, res, next) => {
+  if (req.path.endsWith('.js')) {
+    res.setHeader('Content-Type', 'application/javascript');
+  }
+  next();
+});
+
+// Redirige todas las rutas a index.html para aplicaciones SPA (Single Page Applications)
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
