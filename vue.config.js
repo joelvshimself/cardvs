bacbd1da29ce1f1c4fb280db1beb8980e585f062
Joel Vargas Reynoso <76189923+joelvshimself@users.noreply.github.com>
2024-10-25 10:18:25 -0600

    allowedHosts: 'all', // Permitir todos los hosts en lugar de disableHostCheck

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   devServer: {
-    port: 8080, // Cambia este número si necesitas otro puerto
-    disableHostCheck: true, // Desactiva la verificación del Host para evitar el error "Invalid Host header"
+    port: 8080,
+    allowedHosts: 'all', // Permitir todos los hosts en lugar de disableHostCheck
   }
 })
